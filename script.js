986f398535440ff4c1ec64fd7241d1b75dc97088
Paskkal.IO <74718314+Pasify@users.noreply.github.com>
2025-10-26 18:56:07 +0000

    first completes homepage

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 "use strict";
 const trackerMouse = document.querySelector(".mouse-tracker");
-
+const navToggle = document.getElementById("nav-toggle");
 document.addEventListener("mousemove", (e) => {
   const { clientX: x, clientY: y } = e;
   trackerMouse.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+});
+
+// touch overlay to close nav menu.
+document.querySelector(".nav__overlay").addEventListener("click", () => {
+  navToggle.checked = false;
 });
